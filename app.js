9cbd5eced38c719f49c29f60da84f40cb303ed92
Mufid <Mufidcord@gmail.com>
2026-05-19 14:24:47 +0700

    upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mysql = require('mysql');
 const app = express()
 const path = require('node:path')
+app.disable("x-powered-by");
 require('dotenv').config();
 
 // Import Middleware
